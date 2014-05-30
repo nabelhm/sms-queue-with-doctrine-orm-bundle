4002e02036e9e21e23a9ee34026ffc184639041d
Yosmany Garcia <yosmanyga@gmail.com>
2014-05-30 01:03:18 -0400

    Enabled the configuration of the entities classes parameters

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Cubalider\Bundle\SmsQueueBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('cubalider_sms_queue');
+
+        $rootNode
+            ->children()
+                ->scalarNode('message_class')->cannotBeEmpty()->end()
+                ->scalarNode('bulk_class')->cannotBeEmpty()->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
