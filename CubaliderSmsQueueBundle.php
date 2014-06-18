95deff865bd57e60567d6fdb6933745767c356b3
Yosmany Garcia <yosmanyga@gmail.com>
2014-06-18 19:41:22 -0400

    Added a compiler pass to map doctrine entities

--- CubaliderSmsQueueBundle.php
+++ CubaliderSmsQueueBundle.php
@@ -3,10 +3,18 @@
 namespace Cubalider\Bundle\SmsQueueBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Cubalider\Bundle\SmsQueueBundle\DependencyInjection\Compiler\DoctrineMappingPass;
 
 /**
  * @author Nabel Hernandez <nabelhm@cubalider.com>
  */
 class CubaliderSmsQueueBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new DoctrineMappingPass());
+    }
 }
---
+++ DependencyInjection/Compiler/DoctrineMappingPass.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Cubalider\Bundle\SmsQueueBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+class DoctrineMappingPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $definition = new Definition('Doctrine\ORM\Mapping\Driver\XmlDriver', array(array(
+            sprintf("%s/../../../../../../sms-queue-with-doctrine-orm/src/Cubalider/Component/Sms/Resources/config/doctrine", __DIR__)
+        )));
+        $definition->setPublic(false);
+        $container->setDefinition('cubalider_sms_queue.xml_driver', $definition);
+
+        $definition = $container->getDefinition('doctrine.orm.default_metadata_driver');
+        $definition->addMethodCall('addDriver', array(new Reference('cubalider_sms_queue.xml_driver'), 'Cubalider\Component\Sms\Model'));
+    }
+}
